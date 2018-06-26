a3ea128d2829cd854ae851d54abe4969c1626807
Vinay Sangawar <vinaysangawar@gmail.com>
2018-06-25 22:28:03 -0400

    Initialize Inbox contract and test suite

---
+++ Inbox/test/Inbox.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+describe('Car Class', () => {
+    it('can park', () => {
+        const car = new Car();
+        assert.strictEqual(car.park(), 'stopped');
+    });
+
+    it('can drive', () => {
+
+    });
+});
